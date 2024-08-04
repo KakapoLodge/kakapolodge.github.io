789992a15e8a8461a383f194e9d3a91026d0fd77
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-08-05 11:17:18 +1200

    Add date range picker for accommodation

--- subpages/accommodation_scripts.js
+++ subpages/accommodation_scripts.js
@@ -47,14 +47,17 @@ const TERTIARY_COLOR = "#6e5546";
 const WINDOW_WIDTH = document.documentElement.clientWidth;
 const MOBILE_WIDTH_MAX_NUM_PIXELS = 700;
 
-// const REAL_TIME_RATES_URL = "./mock_little_hotelier_response.json";
-const REAL_TIME_RATES_URL =
-  "https://kakapo-lodge-rates.onrender.com/rates/tonight";
+// const REAL_TIME_RATES_URL = "./mock_rates_response_tonight.json";
+const REAL_TIME_RATES_URL = "https://kakapo-lodge-rates.onrender.com/rates";
 
 const enableAccommodationPage = () => {
   enableMobileFilterButton();
-  enableAccommodationCheckboxes();
-  showRealTimeRates();
+  enableFilterCheckboxes();
+  const dateRangePicker = enableDateRangePicker();
+  showRealTimeRates(dateRangePicker);
+
+  document.getElementById("date-range-picker").onchange = () =>
+    showRealTimeRates(dateRangePicker);
 };
 
 const enableMobileFilterButton = () => {
@@ -89,7 +92,7 @@ const openFilterOptions = (filterButton, filterOptions) => {
   filterOptions.style.display = "flex";
 };
 
-const enableAccommodationCheckboxes = () => {
+const enableFilterCheckboxes = () => {
   const accommodationCheckboxes = document.getElementsByClassName(
     "accommodation-checkbox"
   );
@@ -105,8 +108,7 @@ const filterAccommodation = () => {
   const accommodationCards =
     document.getElementsByClassName("accommodation-card");
 
-  const matchingAccommodation = getMatchingAccommodation(accommodationCards);
-
+  const matchingAccommodation = getMatchingAccommodation();
   showMatchingAccommodation(matchingAccommodation, accommodationCards);
 
   const noAccommodationCard = document.getElementById("no-accommodation");
@@ -127,13 +129,9 @@ const showMatchingAccommodation = (
   }
 };
 
-const getMatchingAccommodation = (accommodationCards) => {
+const getMatchingAccommodation = () => {
   let matchingAccommodation = structuredClone(ALL_ACCOMMODATION);
 
-  matchingAccommodation = filterByAvailableTonight(
-    matchingAccommodation,
-    accommodationCards
-  );
   matchingAccommodation = filterByPrivateRoom(matchingAccommodation);
   matchingAccommodation = filterByPrivateBathroom(matchingAccommodation);
   matchingAccommodation = filterBySeparateBeds(matchingAccommodation);
@@ -199,58 +197,188 @@ const filterBySeparateBeds = (matchingAccommodation) => {
     : matchingAccommodation;
 };
 
-const showRealTimeRates = () => {
-  fetch(REAL_TIME_RATES_URL)
-    .then((response) => response.json())
-    .then(updateLodgeRates);
+const enableDateRangePicker = () => {
+  const todaysDate = getTodaysDate();
+
+  const tomorrowsDate = new Date(todaysDate);
+  tomorrowsDate.setDate(todaysDate.getDate() + 1);
+
+  return new easepick.create({
+    element: document.getElementById("date-range-picker"),
+    css: ["../easepick/bundle/index.css", "../styles.css"],
+    plugins: ["LockPlugin", "RangePlugin"],
+    format: "DD MMM YYYY",
+    LockPlugin: {
+      minDate: new Date(),
+    },
+    RangePlugin: {
+      startDate: todaysDate,
+      endDate: tomorrowsDate,
+      tooltipNumber: (numberOfNights) => numberOfNights - 1,
+      locale: {
+        one: "night",
+        other: "nights",
+      },
+    },
+    setup: (picker) => picker.on("select", (_) => showRealTimeRates(picker)),
+  });
 };
 
-const updateLodgeRates = (lodgeRates) => {
-  for (const accommodationInfo of lodgeRates) {
-    updateLodgeRate(accommodationInfo);
+const showRealTimeRates = (dateRangePicker) => {
+  const [startDateRfc3339, endDateRfc3339] = getDateRange(dateRangePicker);
+  const isStayTonight = checkIsStayTonight(startDateRfc3339, endDateRfc3339);
+
+  fetch(
+    `${REAL_TIME_RATES_URL}?start_date=${startDateRfc3339}&end_date=${endDateRfc3339}`
+  )
+    .then((response) => response.json())
+    .then((ratePlans) => updateAllAccommodationInfo(ratePlans, isStayTonight));
+};
+
+const getDateRange = (dateRangePicker) => {
+  const startDateRfc3339 = convertDateToRfc3339(dateRangePicker.getStartDate());
+  const endDateRfc3339 = convertDateToRfc3339(
+    new Date(dateRangePicker.getEndDate() - 1)
+  );
+
+  return [startDateRfc3339, endDateRfc3339];
+};
+
+const checkIsStayTonight = (startDateRfc3339, endDateRfc3339) => {
+  if (startDateRfc3339 !== endDateRfc3339) {
+    return false;
+  }
+
+  const todaysDate = getTodaysDate();
+  return convertDateToRfc3339(todaysDate) === startDateRfc3339;
+};
+
+const getTodaysDate = () => {
+  const todaysDate = new Date();
+  todaysDate.setHours(0, 0, 0, 0);
+  return todaysDate;
+};
+
+const convertDateToRfc3339 = (date) => {
+  const timezoneOffset = date.getTimezoneOffset() * 60_000;
+  return new Date(date - timezoneOffset).toISOString().split("T")[0];
+};
+
+const updateAllAccommodationInfo = (ratePlans, isStayTonight) => {
+  for (const ratePlan of ratePlans) {
+    updateAccommodationInfo(ratePlan, isStayTonight);
   }
 };
 
-const updateLodgeRate = (lodgeRate) => {
-  const accommodationId = ACCOMMODATION_TYPE_ID_MAP[lodgeRate.name];
+const updateAccommodationInfo = (ratePlan, isStayTonight) => {
+  const accommodationId = ACCOMMODATION_TYPE_ID_MAP[ratePlan.name];
+  const ratePlanDates = ratePlan["rate_plan_dates"];
 
+  const totalPrice = getTotalPrice(ratePlanDates);
+  updatePrice(accommodationId, isStayTonight, totalPrice);
+
+  const [isAvailable, numAvailable] = checkAvailability(ratePlanDates);
+  updateAvailability(accommodationId, isStayTonight, isAvailable, numAvailable);
+  updateBookAccommodationText(
+    accommodationId,
+    isStayTonight,
+    isAvailable,
+    totalPrice
+  );
+};
+
+const getTotalPrice = (ratePlanDates) => {
+  return ratePlanDates
+    .map((ratePlanDate) => ratePlanDate.rate)
+    .reduce((partialSum, rate) => partialSum + rate, 0);
+};
+
+const updatePrice = (accommodationId, isStayTonight, totalPrice) => {
   const accommodationPrice = document.getElementById(
     `${accommodationId}-price`
   );
-  accommodationPrice.innerText = `Tonight: $${lodgeRate.rate}`;
 
-  const numAvailable = lodgeRate["num_available"];
-  const accommodationUnavailable = numAvailable === 0;
+  accommodationPrice.innerText = `${
+    isStayTonight ? "Tonight" : "Total"
+  }: $${totalPrice}`;
+};
 
+const checkAvailability = (ratePlanDates) => {
+  let numAvailable = Number.MAX_SAFE_INTEGER;
+
+  for (const ratePlanDate of ratePlanDates) {
+    if (ratePlanDate["stop_online_sell"] || ratePlanDate.available === 0) {
+      return [false, 0];
+    }
+
+    numAvailable = Math.min(numAvailable, ratePlanDate.available);
+  }
+
+  return [true, numAvailable];
+};
+
+const updateAvailability = (
+  accommodationId,
+  isStayTonight,
+  isAvailable,
+  numAvailable
+) => {
   const bedOrRoom = accommodationId.includes("dorm") ? "bed" : "room";
 
-  let availabilityText = "";
-  if (accommodationUnavailable) {
-    availabilityText = "Sold out for tonight";
-  } else if (numAvailable === 1) {
-    availabilityText = `Only 1 ${bedOrRoom} left tonight!`;
-  } else {
-    availabilityText = `${numAvailable} ${bedOrRoom}s available tonight`;
-  }
+  const availabilityText = getAvailabilityText(
+    isAvailable,
+    numAvailable,
+    isStayTonight,
+    bedOrRoom
+  );
 
   const accommodationAvailability = document.getElementById(
     `${accommodationId}-availability`
   );
+
   accommodationAvailability.innerText = availabilityText;
-  accommodationAvailability.style.color = accommodationUnavailable
+  accommodationAvailability.style.color = isAvailable
     ? TERTIARY_COLOR
     : PRIMARY_COLOR;
+};
 
+const getAvailabilityText = (
+  isAvailable,
+  numAvailable,
+  isStayTonight,
+  bedOrRoom
+) => {
+  if (!isAvailable) {
+    return `Sold out for ${isStayTonight ? "tonight" : "this period"}`;
+  } else if (numAvailable === 1) {
+    return `Only 1 ${bedOrRoom} left  ${
+      isStayTonight ? "tonight" : "for this period"
+    }!`;
+  } else {
+    return `${numAvailable} ${bedOrRoom}s available  ${
+      isStayTonight ? "tonight" : "for this period"
+    }`;
+  }
+};
+
+const updateBookAccommodationText = (
+  accommodationId,
+  isStayTonight,
+  isAvailable,
+  totalPrice
+) => {
   const bookAccommodationText = document.getElementById(
     `book-${accommodationId}-text`
   );
 
-  if (accommodationUnavailable) {
-    bookAccommodationText.innerText = "Book for another night";
-  } else {
+  if (isAvailable) {
     // book direct: 5% off
-    const discountedPrice = (lodgeRate.rate * 0.95).toFixed(2);
+    const discountedPrice = (totalPrice * 0.95).toFixed(2);
     bookAccommodationText.innerText = `Book with us @ $${discountedPrice}`;
+  } else {
+    bookAccommodationText.innerText = `Book for another ${
+      isStayTonight ? "night" : "period"
+    }`;
   }
 };
 
